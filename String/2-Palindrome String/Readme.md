39f80f827d5ae8a82841eda29ead049b29e34612
iankityadav <1002ankit@gmail.com>
2022-04-21 22:24:27 +0530

    solution added of Q2 of string

---
+++ String/2-Palindrome String/Readme.md
@@ -0,0 +1,9 @@
+> **Given a string, write a function to check if it is palindrome or not.**
+
+- A string is said to be palindrome if reverse of the string is same as string. For example, “abba” is palindrome, but “abbc” is not palindrome.
+
+<details>
+<summary>Hint</summary>
+
+- Two pointer approach
+</details>
---
+++ String/2-Palindrome String/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Solution of question 2 of String
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(palindrome("ANNA"));
+    }
+
+    /**
+     * Check whether a string is palindrome or not.
+     * @param s String
+     * @return Return true if the String is palindrome otherwise false
+     */
+    public static boolean palindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
